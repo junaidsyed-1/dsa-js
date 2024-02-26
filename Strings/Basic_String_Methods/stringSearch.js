b47b9969d0f166ea7ce68887cc9526effa4c6b96
junaid <jaanusd79@gmail.com>
2024-02-26 20:06:32 +0530

    Completed string search methods

---
+++ Strings/Basic_String_Methods/stringSearch.js
@@ -0,0 +1,24 @@
+// All of the search JS built-in methods:
+
+const str = "junaid is the best coder";
+
+// 1. indexOf() : returns the index of the first occurrence of a string;
+// console.log(str.indexOf("t"));
+// both these methods accept 2nd parameter as the starting position
+
+// 2. lastIndexOf() : return the index of the last occurrence of a string
+// console.log(str.lastIndexOf("t"));
+
+// 3. search() : returns the index of the first occurrence of a string, but id does not take any other parameter, that means indexOf is more powerful becasuse it can take regex.
+
+// 4. match()  returns an array containing the result of matching a string against a string;
+// console.log(str.match("c"));
+
+// 5. matchAll()
+// console.log(str.matchAll(""));
+
+// 6. includes() : return true if the string contains a specified value;
+// console.log(str.includes("j"));
+
+// 7. startsWith(), 8. endsWith() : return true if the string starts or ends with the specified value;
+// these both methods take second parameter as the starting position;
